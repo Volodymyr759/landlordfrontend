95d87d61b39d37b464e9c2c5282113ef00004edb
Volodia <volodymyr.m@eivolo.com>
2022-05-11 20:21:46 +0300

    styling again

--- src/components/steps/Step7TermsConditions.tsx
+++ src/components/steps/Step7TermsConditions.tsx
@@ -1,14 +1,16 @@
-import { Card, Grid, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import { Card, Grid } from '@mui/material';
 import { useAppDispatch, useAppSelector } from '../../store/redux';
 import { incrementStep, decrementStep } from '../../store/steps/ActionCreators';
 import { FooterTwoBtn } from '../Footers/FooterTwoBtn/FooterTwoBtn';
 import { Header } from '../Header/Header';
 import { MuiGrid } from '../MuiGrid/MuiGrid';
+import { MuiStepper } from '../MuiStepper/MuiStepper';
 import "./styles.css";
 
 export const Step7TermsConditions = () => {
     const dispatch = useAppDispatch();
     const { agreement } = useAppSelector(state => state.agreementReducer);
+    const { stepNumber } = useAppSelector(state => state.stepReducer);
 
     return (
         <MuiGrid appearance='grid-light-gray'>
@@ -16,6 +18,7 @@ export const Step7TermsConditions = () => {
                 onGoBack={() => dispatch(decrementStep())}
                 onPhone={() => console.log('onPhone is not implemented yet')}
             />
+            <MuiStepper activeStep={stepNumber - 5} />
             <p className='p-gray'>PART 2 | Terms and conditions of residential exlusive management agency agreement</p>
             <Grid item xs={12}>
                 <Card className="white-content-wrapper text-centered" >
